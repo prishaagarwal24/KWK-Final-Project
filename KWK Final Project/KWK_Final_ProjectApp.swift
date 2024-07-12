2e5d3116a29eff8441516e81f9885cd8fb5e5d51
prishaagarwal24 <prisha.dev24@gmail.com>
2024-07-12 13:02:04 -0700

    Initial Commit

---
+++ KWK Final Project/KWK_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_Final_ProjectApp.swift
+//  KWK Final Project
+//
+//  Created by Prisha Agarwal on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KWK_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
