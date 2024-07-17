b7850462526d87584a3e3337cee1f1086bb5a740
prishaagarwal24 <prisha.dev24@gmail.com>
2024-07-17 14:19:56 -0700

    update 3

--- BodyLanguage.swift
+++ BodyLanguage.swift
@@ -9,28 +9,57 @@ import SwiftUI
 
 struct BodyLanguage: View {
     var body: some View {
-    HStack {
-        VStack (alignment: .leading) {
-                Text("Body Language:")
-                .font(.largeTitle)
-                .fontWeight(.bold)
+        NavigationStack{
+            HStack {
+                VStack (alignment: .leading) {
+                    Text("Body Language:")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    
+                    Text("The process of communicating nonverbally through conscious or unconscious gestures and movements.")
+                        .font(.title2)
+                }
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(
+                Color.blue
+                    .cornerRadius(10)
+                    .shadow(
+                        color: Color.black.opacity(0.3),
+                        radius: 10,
+                        x: 0.0, y:10)
+            )
+                Text("Don't worry!! Body language can be improved with just a little direction and practice!")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 70)
+                    .frame(width: 350.0)
+                Text("To better help you, please select what you would like to specifically improve:")
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 30)
                 
-                Text("The process of communicating nonverbally through conscious or unconscious gestures and movements.")
+                NavigationLink(destination: EyeContact()) {
+                    Text("Eye Contact")
+                        .padding(10.0)
+                        .tint(.white)
+                        .background(.blue)
+                        .cornerRadius(7)
+                }
+                
+                NavigationLink(destination: HandGestures()) {
+                    Text("Hand Gestures")
+                        .padding(10.0)
+                        .tint(.white)
+                        .background(.blue)
+                        .cornerRadius(7)
+                }
+        
+                Spacer()
             }
         }
-        .padding()
-        .foregroundColor(.white)
-        .background(
-            Color.blue
-                .cornerRadius(10)
-                .shadow(
-                    color: Color.black.opacity(0.3),
-                    radius: 10,
-                    x: 0.0, y:10)
-        )
-        Spacer()
     }
-}
 
 #Preview {
     BodyLanguage()
--- EyeContact.swift
+++ EyeContact.swift
@@ -9,10 +9,73 @@ import SwiftUI
 
 struct EyeContact: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            HStack {
+                VStack (alignment: .leading) {
+                    Text("Eyecontact")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .padding(6.0)
+                    
+                }
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(
+                Color(red: 0/255.0, green: 119/255.0, blue: 182/255.0)
+                    .cornerRadius(10)
+                    .shadow(
+                        color: Color.black.opacity(0.3),
+                        radius: 10,
+                        x: 0.0, y:10)
+            )
+            Text("Eye contact helps you connect with your audience, keep them engaged, and convey your confidence and conviction.")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .frame(width: 350.0)
+                .padding()
+            .foregroundColor(.white)
+            .background(
+                Color(red: 0/255.0, green: 180/255.0, blue: 216/255.0)
+                    .cornerRadius(10)
+                    .shadow(
+                        color: Color.black.opacity(0.3),
+                        radius: 10,
+                        x: 0.0, y:10)
+            )
+            VStack{
+                Text("Quick Tips:")
+                Text(" • If you are having trouble moving your eyes across different faces, span across the back wall!")
+                    .multilineTextAlignment(.leading)
+                Text(" • It’s always a good idea to look towards the middle of the room as a balance/center")
+                    .multilineTextAlignment(.leading)
+            }
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .frame(width: 350.0)
+                .padding()
+            .foregroundColor(.white)
+            .background(
+                Color(red: 144/255.0, green: 224/255.0, blue: 239/255.0)
+                    .cornerRadius(10)
+                    .shadow(
+                        color: Color.black.opacity(0.3),
+                        radius: 10,
+                        x: 0.0, y:10)
+            )
+            
+            Text("Additional Resources::")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .padding(.top, 30)
+            Link("How to Make Eye Contact with Audience", destination: URL(string:"https://www.youtube.com/watch?v=i--CdKh_dHc&ab_channel=Howcast")!)
+            
+            Spacer()
+        }
     }
+    
 }
-
 #Preview {
     EyeContact()
 }
