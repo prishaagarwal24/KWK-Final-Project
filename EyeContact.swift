b056ffc892040e4f2b16c8a2a15dc48740e9efe2
prishaagarwal24 <prisha.dev24@gmail.com>
2024-07-17 14:19:43 -0700

    update 3

---
+++ EyeContact.swift
@@ -0,0 +1,18 @@
+//
+//  EyeContact.swift
+//  KWK Final Project
+//
+//  Created by Prisha Agarwal on 7/17/24.
+//
+
+import SwiftUI
+
+struct EyeContact: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EyeContact()
+}
---
+++ HandGestures.swift
@@ -0,0 +1,18 @@
+//
+//  HandGestures.swift
+//  KWK Final Project
+//
+//  Created by Prisha Agarwal on 7/17/24.
+//
+
+import SwiftUI
+
+struct HandGestures: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HandGestures()
+}
